9e7fb222d461de3dc4242c16130667b5dd021dbc
Matthijs <matthijs.klasens@gmail.com>
2024-06-27 01:37:55 +0200

    Added files

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+
+  description = "First flake";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-23.05";
+  };
+
+  outputs = { self, nixpkgs, ... }:
+    let
+      lib = nixpkgs.lib;
+    in {
+    nixosConfigurations = {
+      nixos = lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [ ./configuration.nix ];
+      };
+    };
+  };
+
+}
