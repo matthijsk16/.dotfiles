9c3f4c487d7ca63c567680b12e7b4d3879f3a7bb
Matthijs <matthijs.klasens@gmail.com>
2024-07-03 00:40:25 +0200

    Small error again...

--- flake.nix
+++ flake.nix
@@ -11,7 +11,7 @@
     };
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }:
+  outputs = { self, nixpkgs, home-manager, ... }@inputs:
     let
       username = "matthijs";
       system = "x86_64-linux";
