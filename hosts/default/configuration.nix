b95892f89495cccac78905c92a95edeeda854571
matthijsk16 <matthijs.klasens@gmail.com>
2024-08-05 18:15:10 +0200

    cleanup

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -7,10 +7,6 @@
 {
   systemd.oomd.enable = false;
 
-  # Bootloader.
-  # boot.loader.systemd-boot.enable = true;
-  # boot.loader.efi.canTouchEfiVariables = true;
-
   boot.loader = {
     efi = {
       canTouchEfiVariables = true;
@@ -22,12 +18,6 @@
       efiSupport = true;
     };
   };
-
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Enable networking
   networking.networkmanager.enable = true;
