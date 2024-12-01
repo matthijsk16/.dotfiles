d140de4f5d22298d3983c9972ae03251832f603d
Matthijs Klasens <matthijs.klasens@gmail.com>
2024-12-01 01:35:49 +0100

    Added hyprland

--- hosts/PC-Matthijs/home.nix
+++ hosts/PC-Matthijs/home.nix
@@ -6,6 +6,10 @@
   home.username = "matthijs";
   home.homeDirectory = "/home/matthijs";
 
+  wayland.windowManager.hyprland = {
+    enable = true;
+  };
+
   # This value determines the Home Manager release that your configuration is
   # compatible with. This helps avoid breakage when a new Home Manager release
   # introduces backwards incompatible changes.
